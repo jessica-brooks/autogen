39bea90c832c2ba431fa996fbc0e3b4c82c00df6
Puraskar Sapkota <puraskar.sapkota@futurenet.com>
2023-03-16 11:19:29 +0000

    Added connection to ChatGPT

--- api/controllers/TitleController.go
+++ api/controllers/TitleController.go
@@ -9,34 +9,27 @@ import (
 	"fmt"
 	"os"
 
-	openai "github.com/openai/openai-go/v2"
+	openai "github.com/sashabaranov/go-openai"
 )
 
-// GetOfferCode returns the offer code and updates the offers
-// with site id
+// get titles from chatGPT
 func (server *Server) GetTitles(w http.ResponseWriter, r *http.Request) {
 	urlParams := r.URL.Query()
 
 	title := urlParams.Get("title")
 	howMany := urlParams.Get("count")
 
-	var titles []string
-	var i int
-
 	//convert string to interger
 	c, _ := strconv.Atoi(howMany)
 
-	for i = 0; i < c; i++ {
-		titles = append(titles, title)
-	}
+	alternateTitles := connectToChatGPTAndGetTitles(title, c)
 
 	w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode(titles)
+	json.NewEncoder(w).Encode(alternateTitles)
 }
 
 func connectToChatGPTAndGetTitles(title string, howMany int) []string {
-
 	var alternateTitles []string
 	apiKey := os.Getenv("OPENAI_API_KEY")
 	if apiKey == "" {
@@ -44,26 +37,18 @@ func connectToChatGPTAndGetTitles(title string, howMany int) []string {
 		return nil
 	}
 
+	c := openai.NewClient(apiKey)
 	ctx := context.Background()
-	client, err := openai.NewClient(apiKey)
-	if err != nil {
-		fmt.Printf("Failed to create client: %v\n", err)
-		return nil
-	}
 
-	prompt := "The quick brown fox jumps over the lazy dog."
-	model := "text-davinci-002"
-	params := &openai.CompletionParams{
-		Prompt:      prompt,
-		MaxTokens:   50,
-		Temperature: 0.7,
-		N:           1,
-		Stop:        []string{"\n"},
+	req := openai.CompletionRequest{
+		Model:     openai.GPT3Ada,
+		MaxTokens: 5,
+		Prompt:    title,
+		N:         howMany,
 	}
-
-	resp, err := client.Completions.Create(ctx, model, params)
+	resp, err := c.CreateCompletion(ctx, req)
 	if err != nil {
-		fmt.Printf("Failed to generate completions: %v\n", err)
+		fmt.Printf("Completion error: %v\n", err)
 		return nil
 	}
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/future/automate-similar-text-generation
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sashabaranov/go-openai v1.5.2 // indirect
+)
